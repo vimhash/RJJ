e28fb8f08d82ed1d4190f5d85edd6737ab81d2b9
Johao Nicolas Perlaza Zambrano <perlazajohao@gmail.com>
2019-07-27 13:46:50 -0500

    demo2

--- Johao/animaciones/src/app/app-routing.module.ts
+++ Johao/animaciones/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { Demo1Component } from './demo1/demo1.component';
+import { Demo2Component } from './demo2/demo2.component';
 
 
 const routes: Routes = [
   { path: 'demo1', component: Demo1Component },
-  // { path: 'demo2', component: Demo1Component },
+  { path: 'demo2', component: Demo2Component },
   // { path: 'demo3', component: Demo1Component }
 ];
 
--- Johao/animaciones/src/app/app.module.ts
+++ Johao/animaciones/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { Demo1Component } from './demo1/demo1.component';
+import { Demo2Component } from './demo2/demo2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    Demo1Component
+    Demo1Component,
+    Demo2Component
   ],
   imports: [
     BrowserModule,
